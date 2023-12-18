3c5dfe2a987f22c10b0de51488c5a2f1b83a2112
r4z4 <r4z4aa@gmail.com>
2023-12-18 04:10:10 +0000

    psql conn

--- basic_concurrency/cmd/main.go
+++ basic_concurrency/cmd/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"basic_concurrency/store"
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"gitlab.com/idoko/bucketeer/handler"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -124,7 +133,55 @@ func DeleteConsultant(w http.ResponseWriter, r *http.Request) {
 	respondwithJSON(w, http.StatusOK, map[string]string{"message": "successfully deleted"})
 }
 
+// func main() {
+// 	routers()
+// 	http.ListenAndServe(":8005", Logger())
+// }
+
 func main() {
-	routers()
-	http.ListenAndServe(":8005", Logger())
+	addr := ":8080"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Error occurred: %s", err.Error())
+	}
+	dbUser, dbPassword, dbName :=
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	httpHandler := handler.NewHandler(db)
+	server := &http.Server{
+		Handler: httpHandler,
+	}
+	go func() {
+		server.Serve(listener)
+	}()
+	defer Stop(server)
+	log.Printf("Started server on %s", addr)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(fmt.Sprint(<-ch))
+	log.Println("Stopping API server.")
+}
+func Stop(server *http.Server) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Could not shut down server correctly: %v\n", err)
+		os.Exit(1)
+	}
 }
--- basic_concurrency/store/consultant_store.go
+++ basic_concurrency/store/consultant_store.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Consultant struct {
-	Id              uuid.UUID `db:"id"`
-	Slug            string    `db:"slug"`
-	ConsultantFName string    `db:"consultant_f_name"`
-	ConsultantLName string    `db:"consultant_l_name"`
-	ImgPath         string    `db:"img_path"`
-	CreatedAt       time.Time `db:"created_at"`
-	UpdatedAt       time.Time `db:"updated_at"`
+	Id              uuid.UUID `db:"id" json:"id"`
+	Slug            string    `db:"slug" json:"slug"`
+	ConsultantFName string    `db:"consultant_f_name" json:"consultant_f_name"`
+	ConsultantLName string    `db:"consultant_l_name" json:"consultant_l_name"`
+	ImgPath         string    `db:"img_path" json:"img_path"`
+	CreatedAt       time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt       time.Time `db:"updated_at" json:"updated_at"`
 }
