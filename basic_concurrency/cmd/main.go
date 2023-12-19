5db1190e13922d8d7bacd003cd5e2aa6baf0390d
r4z4 <r4z4aa@gmail.com>
2023-12-19 04:40:02 +0000

    gross

--- basic_concurrency/cmd/main.go
+++ basic_concurrency/cmd/main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"basic_concurrency/store"
 	"context"
 	"database/sql"
-	"encoding/json"
+	"errors"
 	"fmt"
+	"html/template"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
-
-	"gitlab.com/idoko/bucketeer/handler"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -26,21 +24,21 @@ var db *sql.DB
 
 const (
 	dbName = "basic_golang_concurrency"
-	dbPass = "password"
+	dbPass = "postgres"
 	dbUser = "postgres"
-	dbHost = "localhost"
+	dbHost = "db"
 	dbPort = 5432
 )
 
-func routers() *chi.Mux {
-	router.Get("/consultants", AllConsultants)
-	router.Get("/consultants/{id}", DetailConsultant)
-	router.Post("/consultants", CreateConsultant)
-	router.Put("/consultants/{id}", UpdateConsultant)
-	router.Delete("/consultants/{id}", DeleteConsultant)
+// func routers() *chi.Mux {
+// 	router.Get("/consultants", AllConsultants)
+// 	router.Get("/consultants/{id}", DetailConsultant)
+// 	router.Post("/consultants", CreateConsultant)
+// 	router.Put("/consultants/{id}", UpdateConsultant)
+// 	router.Delete("/consultants/{id}", DeleteConsultant)
 
-	return router
-}
+// 	return router
+// }
 
 // username:password@protocol(address)/dbname?param=value
 func init() {
@@ -62,76 +60,76 @@ func init() {
 	fmt.Println("Successfully connected!")
 }
 
-func AllConsultants(w http.ResponseWriter, r *http.Request) {
-	query, err := db.Prepare("Select * FROM consultants")
-	catch(err)
+// func AllConsultants(w http.ResponseWriter, r *http.Request) {
+// 	query, err := db.Prepare("Select * FROM consultants")
+// 	catch(err)
 
-	_, er := query.Exec()
-	catch(er)
-	defer query.Close()
+// 	_, er := query.Exec()
+// 	catch(er)
+// 	defer query.Close()
 
-	respondwithJSON(w, http.StatusOK, map[string]string{"message": "successfully fetched all"})
-}
+// 	respondwithJSON(w, http.StatusOK, map[string]string{"message": "successfully fetched all"})
+// }
 
-func DetailConsultant(w http.ResponseWriter, r *http.Request) {
-	var consultant store.Consultant
-	id := chi.URLParam(r, "id")
-	json.NewDecoder(r.Body).Decode(&consultant)
+// func DetailConsultant(w http.ResponseWriter, r *http.Request) {
+// 	var consultant store.Consultant
+// 	id := chi.URLParam(r, "id")
+// 	json.NewDecoder(r.Body).Decode(&consultant)
 
-	query, err := db.Prepare("Select * FROM consultants where id=?")
-	catch(err)
-	_, er := query.Exec(id)
-	catch(er)
+// 	query, err := db.Prepare("Select * FROM consultants where id=?")
+// 	catch(err)
+// 	_, er := query.Exec(id)
+// 	catch(er)
 
-	defer query.Close()
+// 	defer query.Close()
 
-	respondwithJSON(w, http.StatusOK, map[string]string{"message": "Selected detail successfully"})
-}
+// 	respondwithJSON(w, http.StatusOK, map[string]string{"message": "Selected detail successfully"})
+// }
 
-// CreatePost create a new post
-func CreateConsultant(w http.ResponseWriter, r *http.Request) {
-	var consultant store.Consultant
-	json.NewDecoder(r.Body).Decode(&consultant)
+// // CreatePost create a new post
+// func CreateConsultant(w http.ResponseWriter, r *http.Request) {
+// 	var consultant store.Consultant
+// 	json.NewDecoder(r.Body).Decode(&consultant)
 
-	query, err := db.Prepare("Insert consultants SET id=?, slug=?")
-	catch(err)
+// 	query, err := db.Prepare("Insert consultants SET id=?, slug=?")
+// 	catch(err)
 
-	_, er := query.Exec(consultant.Id, consultant.Slug)
-	catch(er)
-	defer query.Close()
+// 	_, er := query.Exec(consultant.Id, consultant.Slug)
+// 	catch(er)
+// 	defer query.Close()
 
-	respondwithJSON(w, http.StatusCreated, map[string]string{"message": "successfully created"})
-}
+// 	respondwithJSON(w, http.StatusCreated, map[string]string{"message": "successfully created"})
+// }
 
-// UpdateConsultant update a  spesific Consultant
-func UpdateConsultant(w http.ResponseWriter, r *http.Request) {
-	var consultant store.Consultant
-	id := chi.URLParam(r, "id")
-	json.NewDecoder(r.Body).Decode(&consultant)
+// // UpdateConsultant update a  spesific Consultant
+// func UpdateConsultant(w http.ResponseWriter, r *http.Request) {
+// 	var consultant store.Consultant
+// 	id := chi.URLParam(r, "id")
+// 	json.NewDecoder(r.Body).Decode(&consultant)
 
-	query, err := db.Prepare("Update consultants set id=?, slug=? where id=?")
-	catch(err)
-	_, er := query.Exec(consultant.Id, consultant.Slug, id)
-	catch(er)
+// 	query, err := db.Prepare("Update consultants set id=?, slug=? where id=?")
+// 	catch(err)
+// 	_, er := query.Exec(consultant.Id, consultant.Slug, id)
+// 	catch(er)
 
-	defer query.Close()
+// 	defer query.Close()
 
-	respondwithJSON(w, http.StatusOK, map[string]string{"message": "update successfully"})
+// 	respondwithJSON(w, http.StatusOK, map[string]string{"message": "update successfully"})
 
-}
+// }
 
-// DeleteConsultant remove a spesific Consultant
-func DeleteConsultant(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
+// // DeleteConsultant remove a spesific Consultant
+// func DeleteConsultant(w http.ResponseWriter, r *http.Request) {
+// 	id := chi.URLParam(r, "id")
 
-	query, err := db.Prepare("delete from consultants where id=?")
-	catch(err)
-	_, er := query.Exec(id)
-	catch(er)
-	query.Close()
+// 	query, err := db.Prepare("delete from consultants where id=?")
+// 	catch(err)
+// 	_, er := query.Exec(id)
+// 	catch(er)
+// 	query.Close()
 
-	respondwithJSON(w, http.StatusOK, map[string]string{"message": "successfully deleted"})
-}
+// 	respondwithJSON(w, http.StatusOK, map[string]string{"message": "successfully deleted"})
+// }
 
 // func main() {
 // 	routers()
@@ -140,14 +138,14 @@ func DeleteConsultant(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	addr := ":8080"
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("Error occurred: %s", err.Error())
-	}
-	dbUser, dbPassword, dbName :=
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB")
+	// listener, err := net.Listen("tcp", addr)
+	// if err != nil {
+	// 	log.Fatalf("Error occurred: %s", err.Error())
+	// }
+	// dbUser, dbPass, dbName :=
+	// 	os.Getenv("POSTGRES_USER"),
+	// 	os.Getenv("POSTGRES_PASSWORD"),
+	// 	os.Getenv("POSTGRES_DB")
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
 	db, err := sql.Open("postgres", psqlInfo)
@@ -163,14 +161,27 @@ func main() {
 
 	fmt.Println("Successfully connected!")
 
-	httpHandler := handler.NewHandler(db)
-	server := &http.Server{
-		Handler: httpHandler,
+	http.Handle("/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", serveTemplate)
+
+	err = http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
 	}
-	go func() {
-		server.Serve(listener)
-	}()
-	defer Stop(server)
+
+	// httpHandler := handler.NewHandler(db)
+	// server := &http.Server{
+	// 	Handler: httpHandler,
+	// }
+	// go func() {
+	// 	server.Serve(listener)
+	// }()
+	// defer Stop(server)
 	log.Printf("Started server on %s", addr)
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
@@ -184,4 +195,12 @@ func Stop(server *http.Server) {
 		log.Printf("Could not shut down server correctly: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
